9ac5f45b2ddb7b0054cacca068e1b2df2ba97ca5
Bryan Hann <nobody@nowhere>
2025-06-09 13:58:18 -0230

    Use [bch:000:linkall].

--- .bch/activate.sh
+++ .bch/activate.sh
@@ -2,22 +2,17 @@
 
 r=$(dirname $(dirname ${BASH_SOURCE[0]}))
 
-rm -f ~/.config/gh
-ln -s ${r}/config.gh ~/.config/gh
 export BCH_CONFIG_GH__root=${r}
 export BCH_CONFIG_GH__init=${r}/.bch/activate.sh
 export BCH_CONFIG_GH__lbin=${r}/.bch/lbin
 export BCH_CONFIG_GH__bin=${r}/.bch/bin
 export BCH_CONFIG_GH__lib=${r}/.bch/lib
 
-for s in $(ls ${r}/.bch/lbin/bch*); do
-     d=${HOME}/.local/bin/$(basename $s)
-     [ ! -f ${d} ] && echo "[cookiecutter.dotted] linking ${s}"
-     [ ! -f ${d} ] && ln -s ${s} ${d}
-done
+rm -f ~/.config/gh
+ln -s ${r}/config.gh ~/.config/gh
+
+bch:000:linkall ${r}/.bch/lbin
 
 source ${r}/.bch/init/init.sh
 
 unset r
-unset s
-unset d
